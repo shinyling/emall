79fe684110d88d7fc4c35df04f4f35590a536820
shiny <shinyling123400@163.com>
2018-09-05 17:44:20 +0800

    feign hystrix

--- emall-web-ucenter/src/main/java/com/shiny/emall/ucenter/web/service/UserServiceFallback.java
+++ emall-web-ucenter/src/main/java/com/shiny/emall/ucenter/web/service/UserServiceFallback.java
@@ -1,11 +1,13 @@
 package com.shiny.emall.ucenter.web.service;
 
 import com.shiny.emall.common.ucenter.entity.User;
+import org.springframework.stereotype.Component;
 
 /**
  * @author DELL shiny
  * @create 2018/9/5
  */
+@Component
 public class UserServiceFallback implements UserService {
     @Override
     public User addUser(User user) {
