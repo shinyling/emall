c4e0800915a4983cb40c3699676971e534cf37e4
shiny <shinyling123400@163.com>
2018-09-06 10:27:11 +0800

    zuul 路由熔断及报错

---
+++ emall-api-gateway/src/main/java/com/shiny/emall/api/gateway/fallback/UcenterWebFallbackProvider.java
@@ -0,0 +1,103 @@
+package com.shiny.emall.api.gateway.fallback;
+
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+/**
+ * @author DELL shiny
+ * @create 2018/9/6
+ */
+@Component
+public class UcenterWebFallbackProvider implements FallbackProvider {
+    @Override
+    public ClientHttpResponse fallbackResponse(Throwable throwable) {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return this.getStatusCode().value();
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return this.getStatusCode().getReasonPhrase();
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("内部异常".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                MediaType mt = new MediaType("application","json", Charset.forName("UTF-8"));
+                headers.setContentType(mt);
+                return headers;
+            }
+        };
+    }
+
+    @Override
+    public String getRoute() {
+        return "emall-web-ucenter";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse() {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                MediaType mt = new MediaType("application","json", Charset.forName("UTF-8"));
+                headers.setContentType(mt);
+                return headers;
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("web-ucenter is not response".getBytes());
+            }
+
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                // fallback时的状态码
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return this.getStatusCode().value();
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return this.getStatusCode().getReasonPhrase();
+            }
+
+            @Override
+            public void close() {
+
+            }
+        };
+    }
+
+}
