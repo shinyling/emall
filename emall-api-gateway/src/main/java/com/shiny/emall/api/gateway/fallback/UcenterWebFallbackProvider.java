d9598f845f4b49340c080f8e3ff68520356fddfd
shiny <shinyling123400@163.com>
2018-09-06 14:17:33 +0800

    zuul 路由熔断及报错

--- emall-api-gateway/src/main/java/com/shiny/emall/api/gateway/fallback/UcenterWebFallbackProvider.java
+++ emall-api-gateway/src/main/java/com/shiny/emall/api/gateway/fallback/UcenterWebFallbackProvider.java
@@ -1,5 +1,6 @@
 package com.shiny.emall.api.gateway.fallback;
 
+import com.netflix.hystrix.exception.HystrixTimeoutException;
 import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -20,40 +21,11 @@ import java.nio.charset.Charset;
 public class UcenterWebFallbackProvider implements FallbackProvider {
     @Override
     public ClientHttpResponse fallbackResponse(Throwable throwable) {
-        return new ClientHttpResponse() {
-            @Override
-            public HttpStatus getStatusCode() throws IOException {
-                return HttpStatus.OK;
-            }
-
-            @Override
-            public int getRawStatusCode() throws IOException {
-                return this.getStatusCode().value();
-            }
-
-            @Override
-            public String getStatusText() throws IOException {
-                return this.getStatusCode().getReasonPhrase();
-            }
-
-            @Override
-            public void close() {
-
-            }
-
-            @Override
-            public InputStream getBody() throws IOException {
-                return new ByteArrayInputStream("内部异常".getBytes());
-            }
-
-            @Override
-            public HttpHeaders getHeaders() {
-                HttpHeaders headers = new HttpHeaders();
-                MediaType mt = new MediaType("application","json", Charset.forName("UTF-8"));
-                headers.setContentType(mt);
-                return headers;
-            }
-        };
+        if (throwable instanceof HystrixTimeoutException) {
+            return response(HttpStatus.GATEWAY_TIMEOUT);
+        } else {
+            return response(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Override
@@ -63,6 +35,10 @@ public class UcenterWebFallbackProvider implements FallbackProvider {
 
     @Override
     public ClientHttpResponse fallbackResponse() {
+        return response(HttpStatus.OK);
+    }
+
+    private ClientHttpResponse response(final HttpStatus status){
         return new ClientHttpResponse() {
             @Override
             public HttpHeaders getHeaders() {
@@ -74,13 +50,13 @@ public class UcenterWebFallbackProvider implements FallbackProvider {
 
             @Override
             public InputStream getBody() throws IOException {
-                return new ByteArrayInputStream("web-ucenter is not response".getBytes());
+                return new ByteArrayInputStream("fallback".getBytes());
             }
 
             @Override
             public HttpStatus getStatusCode() throws IOException {
                 // fallback时的状态码
-                return HttpStatus.OK;
+                return status;
             }
 
             @Override
