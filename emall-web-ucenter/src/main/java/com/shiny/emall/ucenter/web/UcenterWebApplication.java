589c596ef61e5dcd72318ff23fd42e9f3d4b81e7
shiny <shinyling123400@163.com>
2018-09-06 15:29:28 +0800

    服务消费者hystrix dashboard

--- emall-web-ucenter/src/main/java/com/shiny/emall/ucenter/web/UcenterWebApplication.java
+++ emall-web-ucenter/src/main/java/com/shiny/emall/ucenter/web/UcenterWebApplication.java
@@ -2,13 +2,17 @@ package com.shiny.emall.ucenter.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * @author DELL shiny
  * @create 2018/9/4
  */
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
